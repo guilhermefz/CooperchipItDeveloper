0d1775b3a3c58c9386f4c2d36d2163824ea9a744
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2025-09-30 09:21:32 -0300

    Organização visual, e adicionado link para o perfil

--- CooperchipItDeveloper/Views/Shared/Components/StatusPaciente/StatusPacienteViewComponent.cs
+++ CooperchipItDeveloper/Views/Shared/Components/StatusPaciente/StatusPacienteViewComponent.cs
@@ -21,16 +21,21 @@ namespace CooperchipItDeveloper.Mvc.Views.Shared.Components.StatusPaciente
         public async Task<IViewComponentResult> InvokeAsync(string estado)
         {
             string card = "";
-            string corProgress = "";
+            string iconLg = "";
             if (estado == "Grave")
                 card = "card card-stat p-3 bg-red";
+                iconLg = "bi bi-8-circle-fill";
             if (estado == "Crítico")
                 card = "card card-stat p-3 bg-yellow";
+            iconLg = "bi bi-arrow-down-circle-fill";
             if (estado == "Em Observação")
                 card = "card card-stat p-3 bg-blue";
+            iconLg = "bi bi-award-fill";
             if (estado == "Estável")
                 card = "card card-stat p-3 bg-write";
-            
+                iconLg = "bi bi-bag";
+
+
             var percentual = _estadoPacienteService.ObterProgresso(estado);
             int quantidade = await _estadoPacienteService.ContarPacientesPorEstado(estado);
             var model = new StatusPacienteViewModel
@@ -39,7 +44,7 @@ namespace CooperchipItDeveloper.Mvc.Views.Shared.Components.StatusPaciente
                 Quantidade = quantidade,
                 Descricao = estado,
                 ClassContainer = card,
-                Progress = corProgress
+                IconeLg = iconLg
             };
 
             return View(model);
--- CooperchipItDeveloper/obj/Debug/net8.0/CooperchipItDeveloper.Mvc.AssemblyInfo.cs
+++ CooperchipItDeveloper/obj/Debug/net8.0/CooperchipItDeveloper.Mvc.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CooperchipItDeveloper.Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bcfd897fd00b4418fdc510a61c84cfe811a99b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+513e59ed22fbcfa30a05e1eed9827b334ebfaa84")]
 [assembly: System.Reflection.AssemblyProductAttribute("CooperchipItDeveloper.Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CooperchipItDeveloper.Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
