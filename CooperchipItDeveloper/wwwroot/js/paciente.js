93a466c5f0b272858db17ec8c2fb3f31119565ef
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2025-12-05 17:19:07 -0300

    feat: Criação do modal de Pacientes e adicionado rota para index de cids

---
+++ CooperchipItDeveloper/wwwroot/js/paciente.js
@@ -0,0 +1,20 @@
+﻿$(document).on('click', '.details', function (e) {
+    e.preventDefault();
+    var $el = $(this);
+    var id = $el.data('id');
+
+        loadModal("/Paciente/Details" + (id ? "?id=" + id : ""));
+    });
+
+function loadModal(url) {
+    $("#modalPaciente").load(url, function () {
+        if (typeof $().modal === "function") {
+            $("#modalPaciente").modal('show');
+        } else {
+            new bootstrap.Modal(document.getElementById('modalPaciente')).show();
+        }
+    });
+}
+
+
+
