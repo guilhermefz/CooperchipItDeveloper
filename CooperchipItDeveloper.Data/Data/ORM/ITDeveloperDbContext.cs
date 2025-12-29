67172f9cfcdf93efb75f580ce447631fb66fe905
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2025-12-29 08:52:14 -0300

    refactor: Movido ModelBuilderExtensions para Data.Seeds sem alterar comportamento

--- CooperchipItDeveloper.Data/Data/ORM/ITDeveloperDbContext.cs
+++ CooperchipItDeveloper.Data/Data/ORM/ITDeveloperDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Cooperchip.ItDeveloper.Domain.Entities;
+using CooperchipItDeveloper.Data.Seeds;
 using CooperchipItDeveloper.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection;
 
 namespace Cooperchip.ItDeveloper.Data.Data.ORM
 {
@@ -36,74 +36,6 @@ namespace Cooperchip.ItDeveloper.Data.Data.ORM
             }
 
             base.OnModelCreating(modelBuilder);
-        }
-    }
-
-
-    public static class ModelBuilderExtensions
-    {
-        public static ModelBuilder AddGenericos(this ModelBuilder builder)
-        {
-            var k = 0;
-            string line;
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "docs", "Generico.csv");
-
-            using (var fs = File.OpenRead(filePath))
-            using (var reader = new StreamReader(fs))
-            {
-                while ((line = reader.ReadLine()) != null)
-                {
-                    var parts = line.Split(';');
-                    var codigo = parts[0];
-                    var generico = parts[1];
-                    if (k > 0)
-                    {
-                        builder.Entity<Generico>().HasData(
-                            new Generico
-                            {
-                                Codigo = Convert.ToInt32(codigo),
-                                Nome = generico
-                            });
-                    }
-                    k++;
-                }
-            }
-
-            return builder;
-        }
-
-
-        public static ModelBuilder AddCids(this ModelBuilder builder)
-        {
-            var k = 0;
-            string line;
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "docs", "Cid.csv");
-
-            using (var fs = File.OpenRead(filePath))
-            using (var reader = new StreamReader(fs))
-            {
-                while ((line = reader.ReadLine()) != null)
-                {
-                    var parts = line.Split(';');
-                    var cidinternalid = parts[0];
-                    var codigo = parts[1];
-                    var diagnostico = parts[2];
-
-                    if (k > 0)
-                    {
-                        builder.Entity<Cid>().HasData(
-                            new Cid
-                            {
-                                CidInternalId = Convert.ToInt32(cidinternalid),
-                                Codigo = codigo,
-                                Diagnostico = diagnostico
-                            });
-                    }
-                    k++;
-                }
-            }
-
-            return builder;
         }
     }
 }
---
+++ CooperchipItDeveloper.Data/Seeds/ModelBuilderExtensions.cs
@@ -0,0 +1,72 @@
+﻿using CooperchipItDeveloper.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CooperchipItDeveloper.Data.Seeds
+{
+    public static class ModelBuilderExtensions
+    {
+        public static ModelBuilder AddGenericos(this ModelBuilder builder)
+        {
+            var k = 0;
+            string line;
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "docs", "Generico.csv");
+
+            using (var fs = File.OpenRead(filePath))
+            using (var reader = new StreamReader(fs))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split(';');
+                    var codigo = parts[0];
+                    var generico = parts[1];
+                    if (k > 0)
+                    {
+                        builder.Entity<Generico>().HasData(
+                            new Generico
+                            {
+                                Codigo = Convert.ToInt32(codigo),
+                                Nome = generico
+                            });
+                    }
+                    k++;
+                }
+            }
+
+            return builder;
+        }
+
+
+        public static ModelBuilder AddCids(this ModelBuilder builder)
+        {
+            var k = 0;
+            string line;
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "docs", "Cid.csv");
+
+            using (var fs = File.OpenRead(filePath))
+            using (var reader = new StreamReader(fs))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split(';');
+                    var cidinternalid = parts[0];
+                    var codigo = parts[1];
+                    var diagnostico = parts[2];
+
+                    if (k > 0)
+                    {
+                        builder.Entity<Cid>().HasData(
+                            new Cid
+                            {
+                                CidInternalId = Convert.ToInt32(cidinternalid),
+                                Codigo = codigo,
+                                Diagnostico = diagnostico
+                            });
+                    }
+                    k++;
+                }
+            }
+
+            return builder;
+        }
+    }
+}
